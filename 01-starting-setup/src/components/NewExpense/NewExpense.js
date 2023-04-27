93511b1ac535894793d440bac35ae1f438a961a7
Vinleon Ang <javadripdev@gmail.com>
2023-04-27 17:24:22 +0800

    Import ExpenseForm component

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,5 +1,12 @@
 import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {};
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
 
 export default NewExpense;
