8aa04a7f383b013f823ba66cc26061bd894a40b4
Vinleon Ang <javadripdev@gmail.com>
2023-04-27 22:19:12 +0800

    Add titleChangeHandler

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,15 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // Whenever an event listener is triggered, an event object is passed to the event handler function
+  const titleChangeHandler = () => {};
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
