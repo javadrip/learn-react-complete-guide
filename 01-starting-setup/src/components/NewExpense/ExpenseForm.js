68717273dadc104d892ce2182d128aed8d819dc5
Vinleon Ang <javadripdev@gmail.com>
2023-05-02 18:35:28 +0800

    Add comments

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,53 +1,74 @@
+import { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   // Multiple states
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   // Single state
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   // Multiple states
   // Whenever an event listener is triggered, an event object is passed to the event handler function
-  // const titleChangeHandler = event => {
-  //   setEnteredTitle(event.target.value);
-  // };
-
-  // const amountChangeHandler = event => {
-  //   setEnteredAmount(event.target.value);
-  // };
-
-  // const dateChangeHandler = event => {
-  //   setEnteredDate(event.target.value);
-  // };
-
-  // Single state
   const titleChangeHandler = event => {
-    setUserInput({
-      ...userInput, // Spread operator to copy the existing state
-      enteredTitle: event.target.value, // Overwrite the enteredTitle property
-    });
+    setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = event => {
-    setUserInput({
-      ...userInput, // Spread operator to copy the existing state
-      enteredAmount: event.target.value, // Overwrite the enteredAmount property
-    });
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = event => {
-    setUserInput({
-      ...userInput, // Spread operator to copy the existing state
-      enteredDate: event.target.value, // Overwrite the enteredDate property
-    });
+    setEnteredDate(event.target.value);
   };
+
+  // Single state, but not ideal and not a good practice, especially when there are edge cases
+  // const titleChangeHandler = event => {
+  //   setUserInput({
+  //     ...userInput, // Spread operator to copy the existing state
+  //     enteredTitle: event.target.value, // Overwrite the enteredTitle property
+  //   });
+  // };
+
+  // const amountChangeHandler = event => {
+  //   setUserInput({
+  //     ...userInput, // Spread operator to copy the existing state
+  //     enteredAmount: event.target.value, // Overwrite the enteredAmount property
+  //   });
+  // };
+
+  // const dateChangeHandler = event => {
+  //   setUserInput({
+  //     ...userInput, // Spread operator to copy the existing state
+  //     enteredDate: event.target.value, // Overwrite the enteredDate property
+  //   });
+  // };
+
+  // Single state: This is recommended when the new state depends on the previous state
+  // const titleChangeHandler = event => {
+  //   setUserInput(prevState => {
+  //     return { ...prevState, enteredTitle: event.target.value };
+  //   });
+  // };
+
+  // const amountChangeHandler = event => {
+  //   setUserInput(prevState => {
+  //     return { ...prevState, enteredAmount: event.target.value };
+  //   });
+  // };
+
+  // const dateChangeHandler = event => {
+  //   setUserInput(prevState => {
+  //     return { ...prevState, enteredDate: event.target.value };
+  //   });
+  // };
 
   return (
     <form>
