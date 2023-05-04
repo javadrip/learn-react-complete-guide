a5a4d9ad0c35d26d09a42b0c1b1467fc3d529f6e
Vinleon Ang <javadripdev@gmail.com>
2023-05-04 18:55:38 +0800

    Add form fields

---
+++ adding-user-practice/src/components/Users/AddUser.js
@@ -0,0 +1,13 @@
+const AddUser = () => {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
