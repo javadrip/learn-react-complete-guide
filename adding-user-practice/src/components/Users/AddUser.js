1d5743df81bf8a7a5da9d9181492ce299b0240d5
Vinleon Ang <javadripdev@gmail.com>
2023-05-04 21:54:35 +0800

    Add style to Card

--- adding-user-practice/src/components/Users/AddUser.js
+++ adding-user-practice/src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from "../UI/Card";
+
 const AddUser = () => {
   const addUserHandler = event => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
