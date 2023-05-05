b5d6913c8c36009faf0a30ba57b9af605a4a8663
Vinleon Ang <javadripdev@gmail.com>
2023-05-05 09:20:15 +0800

    Add list of users

---
+++ adding-user-practice/src/components/Users/UsersList.js
@@ -0,0 +1,17 @@
+import Card from "../UI/Card";
+
+const UsersList = props => {
+  return (
+    <Card>
+      <ul>
+        {props.users.map(user => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
