6f8ef74d62bb4646b2acece4d09828416c8b0aeb
Vinleon Ang <javadripdev@gmail.com>
2023-05-07 22:28:10 +0800

    Add AuthContext.Provider values

--- dummy-log-in/src/App.js
+++ dummy-log-in/src/App.js
@@ -34,7 +34,12 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
