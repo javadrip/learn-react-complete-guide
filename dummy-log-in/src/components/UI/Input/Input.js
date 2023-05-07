613dc2755f7d712e89d605fb3a361ecba97220b8
Vinleon Ang <javadripdev@gmail.com>
2023-05-07 23:21:02 +0800

    Refactor code

--- dummy-log-in/src/components/UI/Input/Input.js
+++ dummy-log-in/src/components/UI/Input/Input.js
@@ -1,21 +1,23 @@
+import classes from "./Input.module.css";
+
 const Input = props => {
   return (
     <div
       className={`${classes.control} ${
         // emailIsValid === false ? classes.invalid : ""
         // If using useReducer hook
-        emailState.isValid === false ? classes.invalid : ""
+        props.isValid === false ? classes.invalid : ""
       }`}
     >
-      <label htmlFor="email">E-Mail</label>
+      <label htmlFor={props.id}>{props.label}</label>
       <input
         type="email"
         id="email"
         // value={enteredEmail}
         // If using useReducer hook
-        value={emailState.value}
-        onChange={emailChangeHandler}
-        onBlur={validateEmailHandler}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
       />
     </div>
   );
