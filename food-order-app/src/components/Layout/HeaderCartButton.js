09994a106e8221d96896136a0965b3ce9f5247a1
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 16:44:48 +0800

    Add styles to elements

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,14 @@
+import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = props => {
   return (
-    <button>
-      <span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span>3</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
