1ac4fefae034174102c5ad788fb375107ca63906
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 15:23:12 +0800

    Rectify image and header styles

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -5,11 +5,11 @@ import mealsImage from "../../assets/food-spread.jpg";
 const Header = props => {
   return (
     <Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
       </header>
-      <div className={classes.header}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
