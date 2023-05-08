8c929f6d6f342a401dc6342d5591626e830fe807
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 15:33:49 +0800

    Add cart icon

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,11 @@
+import CartIcon from "../Cart/CartIcon";
+
 const HeaderCartButton = props => {
   return (
     <button>
-      <span></span>
+      <span>
+        <CartIcon />
+      </span>
       <span></span>
       <span></span>
     </button>
