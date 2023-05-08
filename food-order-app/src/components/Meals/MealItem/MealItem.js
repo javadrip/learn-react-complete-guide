6e71c3ea4f5599f4b6eecb55b126590a594f239f
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 17:41:54 +0800

    Pass props to component

---
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,16 @@
+const MealItems = props => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li>
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.description}</div>
+        <div>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItems;
