78e938e0262d55c6eefd9549006aeff776fa3ec4
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 23:38:20 +0800

    Add add item to cart logic

--- food-order-app/src/context/CartProvider.js
+++ food-order-app/src/context/CartProvider.js
@@ -1,11 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./CartContext";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// This reducer is outside the component because it doesn't need anything from the component and to prevent re-creation on each render
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = props => {
-  const addItemToCartHandler = item => {};
-  const removeItemFromCartHandler = id => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
+    items: cartState.items,
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
