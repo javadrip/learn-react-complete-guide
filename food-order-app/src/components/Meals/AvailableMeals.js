6a40e9a917866938569c9dd40818ae9dce26c237
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 17:06:56 +0800

    Display meal names

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -25,6 +25,14 @@ const DUMMY_MEALS = [
   },
 ];
 
-const AvailableMeals = () => {};
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+
+  return (
+    <section>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
 
 export default AvailableMeals;
