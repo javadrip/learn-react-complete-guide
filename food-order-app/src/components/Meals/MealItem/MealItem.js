3f307fbbff4ffbfa73608efb39adfa4a1d3235af
Vinleon Ang <javadripdev@gmail.com>
2023-05-08 17:44:03 +0800

    Style elements

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,14 @@
+import classes from "./MealItem.module.css";
+
 const MealItems = props => {
   const price = `$${props.price.toFixed(2)}`;
 
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-        <div>{props.description}</div>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{props.description}</div>
         <div>{price}</div>
       </div>
       <div></div>
