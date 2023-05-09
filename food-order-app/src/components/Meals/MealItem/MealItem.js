e81add7b7db4b88de62d343e702b5bc5fe086502
Vinleon Ang <javadripdev@gmail.com>
2023-05-09 15:38:42 +0800

    Add add item to cart functionality

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../context/CartContext";
 
 const MealItems = props => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -13,7 +27,7 @@ const MealItems = props => {
         <div>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
