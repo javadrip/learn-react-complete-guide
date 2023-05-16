6f6020c799894597aa4ae2fbea46a9969ac73579
Vinleon Ang <javadripdev@gmail.com>
2023-05-16 21:23:17 +0800

    Refactor store

---
+++ redux-counter/src/store/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    // createSlice automatically generates the reducer logic and corresponding action creators
+    // The actions can be accessed by authActions.login and authActions.logout
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
---
+++ redux-counter/src/store/counterSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+
+// Using redux toolkit
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    // createSlice automatically generates the reducer logic and corresponding action creators
+    // The actions can be accessed by counterActions.increment, counterActions.decrement, counterActions.increase, and counterActions.toggle
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload; // payload is a special name designated by redux toolkit, and we cannot use other names
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+// Using original redux
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// Using original redux
+// const store = createStore(counterReducer);
+
+export const counterActions = counterSlice.actions;
+export default counterSlice.reducer;
