974f6c57bb1d66f165258157634984d382a0c5fd
Vinleon Ang <javadripdev@gmail.com>
2023-05-19 12:49:28 +0800

    Complete cartSlice logic

--- redux-cart/src/store/cartSlice.js
+++ redux-cart/src/store/cartSlice.js
@@ -22,6 +22,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice += newItem.price;
       }
     },
-    removeItemToCart() {},
+    removeItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
