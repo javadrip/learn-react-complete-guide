6b31529d68fce2ff2721e36d08e508b32b4a893b
Vinleon Ang <javadripdev@gmail.com>
2023-05-19 13:27:17 +0800

    Add decrement logic to removeItemToCart

--- redux-cart/src/store/cartSlice.js
+++ redux-cart/src/store/cartSlice.js
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
       state.changed = true;
       if (!existingItem) {
         state.items.push({
-          id: newItem.id,
+          itemId: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -25,6 +25,8 @@ const cartSlice = createSlice({
     removeItemToCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
